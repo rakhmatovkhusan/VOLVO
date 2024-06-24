4f1de844387f95a8a6a78c7c5e21c1517e2f80d8
khusan rakhmatov <khusanrakhmatov5@gmail.com>
2024-06-24 16:44:06 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,139 @@
+let button1 = document.querySelector(".button_1_first")
+let button2 = document.querySelector(".button_2_second")
+let button3 = document.querySelector(".button_3_third")
+let button4 = document.querySelector(".button_4_fourth")
+let button5 = document.querySelector(".button_5_fifth")
+let button6 = document.querySelector(".button_6_sixth")
+
+let carsfeature1 = document.querySelector(".cars_feature_1")
+let carsfeature2 = document.querySelector(".cars_feature_2")
+let carsfeature3 = document.querySelector(".cars_feature_3")
+let carsfeature4 = document.querySelector(".cars_feature_4")
+let carsfeature5 = document.querySelector(".cars_feature_5")
+let carsfeature6 = document.querySelector(".cars_feature_6")
+let carsfeature7 = document.querySelector(".cars_feature_7")
+let carsfeature8 = document.querySelector(".cars_feature_8")
+let carsfeature9 = document.querySelector(".cars_feature_9")
+let carsfeature10 = document.querySelector(".cars_feature_10")
+let carsdiv = document.querySelector(".cars_div")
+
+button1.addEventListener("click", function(){
+    carsdiv.style.padding = "0 0 0 0";
+    button1.style.borderBottom = "2px solid black";
+    button2.style.borderBottom = "none";
+    button3.style.borderBottom = "none";
+    button4.style.borderBottom = "none";
+    button5.style.borderBottom = "none";
+    button6.style.borderBottom = "none";
+    carsfeature1.style.display = "block";
+    carsfeature2.style.display = "block";
+    carsfeature3.style.display = "block";
+    carsfeature4.style.display = "block";
+    carsfeature5.style.display = "block";
+    carsfeature6.style.display = "block";
+    carsfeature7.style.display = "block";
+    carsfeature8.style.display = "block";
+    carsfeature9.style.display = "block";
+    carsfeature10.style.display = "block";
+})
+
+button2.addEventListener("click", function(){
+     carsdiv.style.padding = "0 37.9%";
+    button1.style.borderBottom = "none";
+    button2.style.borderBottom = "2px solid black";
+    button3.style.borderBottom = "none";
+    button4.style.borderBottom = "none";
+    button5.style.borderBottom = "none";
+    button6.style.borderBottom = "none";
+    carsfeature1.style.display = "block";
+    carsfeature2.style.display = "none";
+    carsfeature3.style.display = "none";
+    carsfeature4.style.display = "none";
+    carsfeature5.style.display = "none";
+    carsfeature6.style.display = "none";
+    carsfeature7.style.display = "none";
+    carsfeature8.style.display = "none";
+    carsfeature9.style.display = "none";
+    carsfeature10.style.display = "none";
+})
+
+button3.addEventListener("click", function(){
+    carsdiv.style.padding = "0 0";
+    button1.style.borderBottom = "none";
+    button2.style.borderBottom = "none";
+    button3.style.borderBottom = "2px solid black";
+    button4.style.borderBottom = "none";
+    button5.style.borderBottom = "none";
+    button6.style.borderBottom = "none";
+    carsfeature1.style.display = "none";
+    carsfeature2.style.display = "block";
+    carsfeature3.style.display = "block";
+    carsfeature4.style.display = "block";
+    carsfeature5.style.display = "block";
+    carsfeature6.style.display = "block";
+    carsfeature7.style.display = "none";
+    carsfeature8.style.display = "none";
+    carsfeature9.style.display = "none";
+    carsfeature10.style.display = "none";
+})
+
+button4.addEventListener("click", function(){
+    carsdiv.style.padding = "0 37.9%";
+    button1.style.borderBottom = "none";
+    button2.style.borderBottom = "none";
+    button3.style.borderBottom = "none";
+    button4.style.borderBottom = "2px solid black";
+    button5.style.borderBottom = "none";
+    button6.style.borderBottom = "none";
+    carsfeature1.style.display = "none";
+    carsfeature2.style.display = "none";
+    carsfeature3.style.display = "none";
+    carsfeature4.style.display = "none";
+    carsfeature5.style.display = "none";
+    carsfeature6.style.display = "none";
+    carsfeature7.style.display = "block";
+    carsfeature8.style.display = "none";
+    carsfeature9.style.display = "none";
+    carsfeature10.style.display = "none";
+})
+
+button5.addEventListener("click", function(){
+    carsdiv.style.padding = "0 25.5%";
+    button1.style.borderBottom = "none";
+    button2.style.borderBottom = "none";
+    button3.style.borderBottom = "none";
+    button4.style.borderBottom = "none";
+    button5.style.borderBottom = "2px solid black";
+    button6.style.borderBottom = "none";
+    carsfeature1.style.display = "none";
+    carsfeature2.style.display = "none";
+    carsfeature3.style.display = "none";
+    carsfeature4.style.display = "none";
+    carsfeature5.style.display = "none";
+    carsfeature6.style.display = "none";
+    carsfeature7.style.display = "none";
+    carsfeature8.style.display = "block";
+    carsfeature9.style.display = "block";
+    carsfeature10.style.display = "none";
+})
+
+button6.addEventListener("click", function(){
+    carsdiv.style.padding = "0 37.9%";
+    button1.style.borderBottom = "none";
+    button2.style.borderBottom = "none";
+    button3.style.borderBottom = "none";
+    button4.style.borderBottom = "none";
+    button5.style.borderBottom = "none";
+    button6.style.borderBottom = "2px solid black";
+    carsfeature1.style.display = "none";
+    carsfeature2.style.display = "none";
+    carsfeature3.style.display = "none";
+    carsfeature4.style.display = "none";
+    carsfeature5.style.display = "none";
+    carsfeature6.style.display = "none";
+    carsfeature7.style.display = "none";
+    carsfeature8.style.display = "none";
+    carsfeature9.style.display = "none";
+    carsfeature10.style.display = "block";
+})
+
